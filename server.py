69b45fe483ac3f7026dcb4793a8e8f913fcf5e94
dd@dd-dev <dd@dd-dev>
2020-09-18 17:29:38 +0300

    maplocator. Add function getMap and update npm

--- server.py
+++ server.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="build/static", template_folder="build")
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 print('Starting Flask!')
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.run(host='0.0.0.0')
